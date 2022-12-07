945f011762f4b95b97e5efc271df8e50f9335c0a
HinanaiTenko <mrorange1030@gmail.com>
2022-12-07 18:45:58 +0100

    login (nie dziala

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Linq.Expressions;
+using System.Text;
+
+string login = null;
+string haslo = null;
+
+Start:
+Console.WriteLine("Wpisz 1, jeśli chcesz stworzyć nowego użytkownika");
+Console.WriteLine("Wpisz 2, jeśli chcesz się zalogować");
+int wybor = Int32.Parse(Console.ReadLine());
+
+        Console.WriteLine("Wpisz login");
+        login = Console.ReadLine();
+        Console.WriteLine(login);
+        Console.WriteLine("Wpisz hasło");
+        haslo = Console.ReadLine();
+        Console.WriteLine(login);
+    string[] lines =
+        {
+            "User "+login,haslo, login
+        };
+
+    await File.WriteAllLinesAsync("WriteLines.txt", lines);
+
+
+
+
+    
