41dabf238ff42a468eb93d11f3c554ef1ab01f5f
HinanaiTenko <mrorange1030@gmail.com>
2022-12-07 19:00:03 +0100

    login

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,20 +11,38 @@ Console.WriteLine("Wpisz 1, jeśli chcesz stworzyć nowego użytkownika");
 Console.WriteLine("Wpisz 2, jeśli chcesz się zalogować");
 int wybor = Int32.Parse(Console.ReadLine());
 
-        Console.WriteLine("Wpisz login");
-        login = Console.ReadLine();
-        Console.WriteLine(login);
-        Console.WriteLine("Wpisz hasło");
-        haslo = Console.ReadLine();
-        Console.WriteLine(login);
+if (wybor == 1)
+{
+    Console.WriteLine("Wpisz login");
+    login = Console.ReadLine();
+    Console.WriteLine(login);
+    Console.WriteLine("Wpisz hasło");
+    haslo = Console.ReadLine();
+    Console.WriteLine(login);
     string[] lines =
         {
-            "User "+login,haslo, login
+            haslo, login
         };
 
-    await File.WriteAllLinesAsync("WriteLines.txt", lines);
+    await File.WriteAllLinesAsync("user" + login + ".txt", lines);
+}
 
+else if (wybor == 2)
+{
+    Console.WriteLine("Wpisz login");
+    login = Console.ReadLine();
+    Console.WriteLine(login);
+    Console.WriteLine("Wpisz hasło");
+    haslo = Console.ReadLine();
+    Console.WriteLine(login);
+    string[] lines =
+        {
+            haslo, login
+        };
 
+    await File.WriteAllLinesAsync("user" + login + ".txt", lines);
+}
 
+else
+    goto Start;
 
-    
