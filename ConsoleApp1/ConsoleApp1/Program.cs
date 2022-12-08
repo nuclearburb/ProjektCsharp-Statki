daf690f1224f2e1b6043a9c09fdaf4368741c6b6
Czechuuuu <johnybi25@gmail.com>
2022-12-08 10:49:57 +0100

    panel admina

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,49 +3,52 @@ using System.IO;
 using System.Linq.Expressions;
 using System.Text;
 
-string typeOfUser = "user";
+string typeOfUser;
 string login;
 string haslo;
 
 
-Start:
+start:
+typeOfUser = "user";
+login = "";
+haslo = "";
+
 Console.WriteLine("Wpisz 1, jesli chcesz stworzyc nowego uzytkownika");
 Console.WriteLine("Wpisz 2, jesli chcesz sie zalogowac");
 int wybor = Int32.Parse(Console.ReadLine());
 
 if (wybor == 1)
 {
-    Console.WriteLine("Wpisz login");
+    Console.Write("Wpisz login: ");
     login = Console.ReadLine();
 
     string sciezkaPliku = "user" + login + ".txt";
     if (File.Exists(sciezkaPliku)) // sprawdzanie czy dane konto juz istnieje
     {
         Console.WriteLine("Dany użytkownik już istnieje");
-        goto Start;
+        goto start;
     }
 
-    Console.WriteLine("Wpisz haslo");
+    Console.Write("Wpisz haslo: ");
     haslo = Console.ReadLine();
     string[] lines =
         {
             login, haslo
         };
 
-    await File.WriteAllLinesAsync("user" + login + ".txt", lines); // Tworzy nowy plik tekstowy dla kazdego uzytkownika 
-    goto Start;
+    File.WriteAllLines("user" + login + ".txt", lines); // Tworzy nowy plik tekstowy dla kazdego uzytkownika 
+    goto start;
 }
 
 else if (wybor == 2)
 {
-    Console.WriteLine("Wpisz login");
+    Console.Write("Wpisz login: ");
     login = Console.ReadLine();
-    Console.WriteLine(login);
     if (login == "admin")
     {
         typeOfUser = "admin";
     };
-    Console.WriteLine("Wpisz haslo");
+    Console.Write("Wpisz haslo: ");
     haslo = Console.ReadLine();
     string[] test = System.IO.File.ReadAllLines(typeOfUser + login + ".txt");  // Uzytkownicy oraz admin znajduja sie jako pliki tekstowe w folderze z programem
     if (test[1] == haslo)
@@ -54,19 +57,78 @@ else if (wybor == 2)
     }
     else
     {
-        goto Start; // wraca na gore
+        Console.Write("Nieprawidlowe haslo!");
+        goto start; // wraca na gore
     };
 }
 
 if (typeOfUser == "admin")
 {
     Console.WriteLine("Witaj adminie");
+
+    panelAdmina:
+
+    Console.WriteLine("1. Podglad calej floty");
+    Console.WriteLine("2. Podglad zawartosci statkow, lokalizacja");
+    Console.WriteLine("3. Dodaj kontener");
+    Console.WriteLine("4. Zarzadzanie statkiem");
+    Console.WriteLine("5. Zmiana danych uzytkownika");
+    Console.WriteLine("6. Wyloguj");
+
     wybor = Int32.Parse(Console.ReadLine());
 
+    switch (wybor)
+    {
+        case 1:
+            {
+                
+            }
+            break;
+        case 2:
+            {
 
+            }
+            break;
+        case 3:
+            {
 
+            }
+            break;
+        case 4:
+            {
 
+            }
+            break;
+        case 5:
+            {
+                Console.Write("Podaj uzytkownika: ");
+                login = Console.ReadLine();
 
+                string sciezkaPliku = "user" + login + ".txt";
+                if (!File.Exists(sciezkaPliku)) // sprawdzanie czy dane konto juz istnieje
+                {
+                    Console.WriteLine("Dany użytkownik nie istnieje");
+                    break;
+                }
+
+                Console.Write("Nowe haslo: ");
+                haslo = Console.ReadLine();
+                string[] lines =
+                    {
+                        login, haslo
+                    };
+
+                File.WriteAllLines("user" + login + ".txt", lines); // Tworzy nowy plik tekstowy dla kazdego uzytkownika 
+            }
+            break;
+        case 6:
+            {
+                goto start;
+            }
+            break;
+    }
+
+    goto panelAdmina;
 }
 
 else
